9a5c770b5127b074660648157a7369764811226a
mhrsenthil <mhrsenthil90@gmail.com>
2026-09-20 15:19:32 +0530

    Create build.gradle.kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,16 @@
+plugins {
+    id("com.android.application")
+}
+
+android {
+    namespace = "com.learnenglishthroughtamil.free"
+    compileSdk = 36
+
+    defaultConfig {
+        applicationId = "com.learnenglishthroughtamil.free"
+        minSdk = 23
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0"
+    }
+}
